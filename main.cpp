fd839400700f273d11192e61fb02bb51faa36351
meghashyaam <meghashyaam15@gmail.com>
2026-01-12 00:07:10 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    cout << "Enter name:";
+    string name;
+    cin >> name;
+    cout << endl << "Welcome " << name;
+    return 0;
+}
